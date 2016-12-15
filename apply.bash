15b9637e7699bf5246dbd7243fce9797c5565171
remotejob <support@mazurov.eu>
2016-12-15 04:24:49 +0200

    dev

--- apply.bash
+++ apply.bash
@@ -33,10 +33,8 @@ if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_ca
 cp config.gcfg.alastonkuvia.info config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
 
-
 cp config.gcfg.antaa.fi config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
-
 
 cp config.gcfg.astrologi.fi config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
@@ -44,29 +42,41 @@ if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_ca
 cp config.gcfg.chatit.fi config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
 
-
 cp config.gcfg.huoneisto.mobi config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
-
 
 cp config.gcfg.ilmais.fi config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
 
-
 cp config.gcfg.kaukotyo.eu config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
-
 
 cp config.gcfg.kvartira-tsentr.eu config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
 
-
 cp config.gcfg.kuplyu.net config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
 
-
 cp config.gcfg.remotejobs.mobi config.gcfg && ./bin/apply_headless
 if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
+
+cp config.gcfg.isot.fi config.gcfg && ./bin/apply_headless
+if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
+
+cp config.gcfg.juutube.fi config.gcfg && ./bin/apply_headless
+if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
+
+cp config.gcfg.kauniita.fi config.gcfg && ./bin/apply_headless
+if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
+
+cp config.gcfg.kertomukset.fi config.gcfg && ./bin/apply_headless
+if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
+
+cp config.gcfg.kiima.fi config.gcfg && ./bin/apply_headless
+if [ -f "./cant_apply.txt" ]; then ./bin/cant_apply;cat cant_apply.txt >> all_cant_apply.txt;rm -f cant_apply.txt;fi
+
+
+
 
 
 
